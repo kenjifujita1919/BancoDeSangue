04eab0e81c7348aa1b30c1c833b108e93c7f966b
kenjifujita1919 <43866316+kenjifujita1919@users.noreply.github.com>
2021-04-21 22:14:48 -0300

    get set
    adicionado get set, os erros estao comentados

---
+++ src/models/Doador.java
@@ -0,0 +1,51 @@
+package models;
+
+public class Doador {
+
+	// Características, atributos ou propriedades
+	String nome;
+	String	cpf;
+	String	email;
+	String	telefone;
+	String	tipo_sanguineo;
+	double	peso;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getTipo_sanguineo() {
+		return tipo_sanguineo;
+	}
+	public void setTipo_sanguineo(String tipo_sanguineo) {
+		this.tipo_sanguineo = tipo_sanguineo;
+	}
+	public double getPeso() {
+		return peso;
+	}
+	public void setPeso(double peso) {
+		this.peso = peso;
+	} 
+	
+	// Getters and Setters
+	
+}
---
+++ src/models/Funcionario.java
@@ -0,0 +1,44 @@
+package models;
+
+public class Funcionario {
+	
+	// Características, atributos ou propriedades
+	double id;
+	String nome;
+	String	cpf;
+	String	email;
+	String	telefone;
+	public double getId() {
+		return id;
+	}
+	public void setId(double id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	// Getters and Setters
+	
+}

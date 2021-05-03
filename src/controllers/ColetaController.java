c2eaf8be63dc196b13e7091e8a478b9c54370231
Andre Luiz Thiel <43017985+andrethiel@users.noreply.github.com>
2021-05-03 16:27:37 -0300

    trabalho

--- src/controllers/ColetaController.java
+++ src/controllers/ColetaController.java
@@ -10,8 +10,6 @@ import views.DoadorView;
 
 public class ColetaController {
 
-	private static	ArrayList<Doador> doadores = new ArrayList<Doador>();
-	private static	ArrayList<Funcionario> funcionarios = new ArrayList<Funcionario>();
 	private static	ArrayList<ColetaDeSangue> coletas = new ArrayList<ColetaDeSangue>();
 		
 	
@@ -20,6 +18,7 @@ public static void Cadastro(String doador, String funcionario) {
 	ColetaDeSangue coleta = new ColetaDeSangue();
 	doadores = DoadorController.doadores;
 	funcionarios = FuncionarioController.funcionarios;
+		
 		for(int i = 0; i < doadores.size(); i++) {
 			Doador d = doadores.get(i);
 			if(d.getNome().equals(doador)) {
